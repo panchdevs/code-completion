83847cdd8e6af629a5a722e292bc69c5a05b718a
Abhijit Katiyar <abhijitkatiyar@gmail.com>
2015-08-30 15:49:17 -0700

    Show match percentage

--- winnow.cpp
+++ winnow.cpp
@@ -8,22 +8,24 @@
 #define q 101
 
 using namespace std;
+int overallTotal,matched,check[101];
 
 void find_min(int *min, int *min_pos, int hashes[],int i,int window)
 {
-    *min = 99999999;
+	*min = 99999999;
     int j = i;
     while(j<(window + i))
     {
-      if(*min>=hashes[j])
-      { *min = hashes[j];
-        *min_pos = j;
-      }
-      j++;
-    }
+		if(*min>=hashes[j])
+		{
+			*min = hashes[j];
+        	*min_pos = j;
+    	}
+    	j++;
+	}
 }
 
-void match(string filex, int pl, int window)
+void match(string filex, int pl, int window, int fileNo)
 {
     int tl;
     ifstream fileOne;
@@ -31,83 +33,100 @@ void match(string filex, int pl, int window)
     string text;
     int k = 0;
     std::map <int, vector<int> > sig;
-    while(getline(fileOne, text))
+	while(getline(fileOne, text))
     {
-      tl = text.size();
-
-      int h = 1;
-      int p = 0;
-      int t = 0;
-      int hashes[tl-pl];
-      for(int i=0; i<pl-1;i++)
-        h = (h*d)%q;
-
-      for(int i=0; i<=pl-1;i++)
-      {
-        //p = (p*d + pat[i])%q;
-        t = (t*d + text[i])%q;
-      }
-      for(int i=0; i< tl-pl; i++)
-      {
-        if(i < tl - pl)
-        {
-           t = (d*(t - text[i]*h) + text[i+pl])%q;
-           if(t < 0) t = t+q;
-           hashes[i] = t;
-          // cout<<hashes[i]<<" ";
-        }
-      }
-     int min_pos = -1, j = 0, min;
-     for(int i=0;i<(tl - pl);i++)
-     {
-        if(i == 0) 
-        { find_min(&min, &min_pos, hashes, i, window);
-        	//cout<<min<<"\n";
-          sig[k].push_back(min);
-        }
-        else if(i < (min_pos + window) )
-        {
-           if(hashes[i] == min) min_pos = i;
-           else if(hashes[i] < min)
-           {
-             min = hashes[i];
-             min_pos = i;
-             //cout<<min<<"\n";
-             sig[k].push_back(min);
-           }
-        }
-        else if(i >= (min_pos + window))
-        {
-          i -= (window - 1) ;
-          find_min(&min, &min_pos, hashes, i, window);
-          //cout<<min<<"\n";
-          sig[k].push_back(min);
-        }
-
-     }
-     k++;
-    }
-
-    cout<<"\nsignature is \n";
-    int count = 0;
-    for(int i=0;i<k;i++)
-    {
-    	cout<<"line no. "<<i<<"\nHashes ";
-    	for(int j = 0;j<sig[i].size();j++)
-    		{ cout<<sig[i][j]<<" "; count++; }
+    	tl = text.size();
+		int h = 1;
+		int p = 0;
+		int t = 0;
+		int hashes[tl-pl];
+		for(int i=0; i<pl-1;i++)
+		h = (h*d)%q;
+		for(int i=0; i<=pl-1;i++)
+		{
+			//p = (p*d + pat[i])%q;
+			t = (t*d + text[i])%q;
+		}
+		for(int i=0; i< tl-pl; i++)
+		{
+			if(i < tl - pl)
+			{
+				hashes[i] = t;
+				t = (d*(t - text[i]*h) + text[i+pl])%q;
+				if(t < 0) t = t+q;
+				// cout<<hashes[i]<<" ";
+	        }
+		}
+    	int min_pos = -1, j = 0, min;
+    	for(int i=0;i<(tl - pl);i++)
+    	{
+    		if(i == 0) 
+    		{
+				find_min(&min, &min_pos, hashes, i, window);
+				//cout<<min<<"\n";
+		  		sig[k].push_back(min);
+			}
+			else if(i < (min_pos + window) )
+			{
+		   		if(hashes[i] == min) min_pos = i;
+		   		else if(hashes[i] < min)
+		   		{
+					min = hashes[i];
+					min_pos = i;
+					//cout<<min<<"\n";
+					sig[k].push_back(min);
+				}
+			}
+			else if(i >= (min_pos + window))
+			{
+				i -= (window - 1) ;
+				find_min(&min, &min_pos, hashes, i, window);
+				//cout<<min<<"\n";
+				sig[k].push_back(min);
+			}
+		}
+		k++;
+	}
+	
+	cout<<"\nsignature is \n";
+	int count = 0;
+	for(int i=0;i<k;i++)
+	{
+		cout<<"line no. "<<i<<"\nHashes ";
+		for(int j = 0;j<sig[i].size();j++)
+		{
+			cout<<sig[i][j]<<" ";
+			count++;
+			if(fileNo==2)
+			{
+				if(check[sig[i][j]])
+				{
+					matched += 2;
+					check[sig[i][j]]--;
+				}
+			}
+			else
+			{
+				check[sig[i][j]]++;
+			}
+		}
     	cout<<"\n";
     }
-    cout<<"total hashes are "<<count<<"\n";
+	cout<<"Total hashes are "<<count<<"\n";
+	overallTotal+=count;
 }
 
-int main() {
+int main()
+{
 
-	string text,pat ; int pl,window;
-	cout<<"enter substring lenght "; cin>>pl;
-	cout<<"enter window size "; cin>>window;
-	for(int i=0;i<2;i++)
-	{ cout<<"enter file 1 "; cin>>text;
-	  match(text,pl,window);
-	 }
+	string fileName,pat ; int pl,window;
+	cout<<"Enter substring length "; cin>>pl;
+	cout<<"Enter window size "; cin>>window;
+	for(int fileNo=1;fileNo<=2;fileNo++)
+	{ 
+		cout<<"Enter file "<<fileNo<<" Name: "; cin>>fileName;
+	  	match(fileName,pl,window,fileNo);
+	}
+	cout<<"Matching Percentage: "<<(float)(matched*100)/overallTotal;
 	return 0;
 }
